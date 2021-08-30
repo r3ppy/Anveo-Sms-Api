ae6f2e6aa686389a7cb969cb98aff56fc1f4eedf
R3ppy <89770271+r3ppy@users.noreply.github.com>
2021-08-30 11:26:22 +0200

    Update pysms.py

--- pysms.py
+++ pysms.py
@@ -4,12 +4,12 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 XINO = open(raw_input("Phone Numbers ? "), "r").read().split("\n")
 
-API = "a3e6815558170a640bb6eaafa1885f4450dda419"
-FROM = "14702062783"
+API = ""
+FROM = ""
 MSG = "Hello World!"
 
 for NUBMER in XINO:
-	r = requests.get("https://bagozasndr.com/sms.php?key={}&from={}&to={}&msg={}".format(API, FROM, NUBMER, MSG)).content
+	r = requests.get("https://example.com/sms.php?key={}&from={}&to={}&msg={}".format(API, FROM, NUBMER, MSG)).content
 	r = eval(r)
 	if str(r['Status']) == "1":
 		New_Message_Count = r['RemainingMessages']
